c4df48856c63bb11a94e7353718755dcb37ca4f7
Adam Keesecker <adamkeesecker@gmail.com>
2014-04-17 21:46:37 -0500

    move Piece implemented, not working. Deselect also functional

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -3,54 +3,81 @@
 
   $(document).ready(init);
 
-  var playerTurn;
-  var player1;
-  var player2;
-  var currentPlayer;
-
-
+  var currPlayer;
 
   function init(){
+    currPlayer='player1';
     addSpace();
     setUpBoard();
-    $('.validSpace').click(selectPiece);
-    $('#reset').click(reset);
+
+    $('#reset').click(request);
+
+    $('#board').on('click','.validSpace', selectPiece);
+    $('#board').on('click','td:not(.piece)', movePiece);
+
+
+
+  }
+
+
+
+
+
+
+  function movePiece(){
+    if($(this).hasClass('validSpace')){
+      $('.selected').removeClass('selected');
+    }
   }
 
   function selectPiece(){
-    if(isSelected()){
-      $('.selected').removeClass('selected');
+    if($(this).hasClass('selected')){
+      $(this).removeClass('selected');
     }
-    $(this).addClass('selected');
+    else{
+      if(isSelected()){
+        $('.selected').removeClass('selected');
+      }
+      if($(this).hasClass('piece')){
+        if($(this).hasClass(currPlayer)){
+          $(this).addClass('selected');
+        }
+      }
+    }
   }
 
   function isSelected(){
-    if($('.selected').length > 0){
-      return true;
-    }
-    return false;
+    return($('.selected').length > 0);
   }
+
+
+  function setUpBoard(){
+    var spaces = $('.validSpace');
+    for(var i=0; i<12; i++){
+      $(spaces[i]).addClass('player2');
+      $(spaces[i]).addClass('piece');
+
+    }
+    for(i=20; i<32; i++){
+      $(spaces[i]).addClass('player1');
+      $(spaces[i]).addClass('piece');
+    }
+  }
+
 
   function addSpace(){
     $('tr:nth-child(2n) td:nth-child(2n+1)').addClass('validSpace');
     $('tr:nth-child(2n-1) td:nth-child(2n)').addClass('validSpace');
   }
 
-  function setUpBoard(){
-    var spaces = $('.validSpace');
-    for(var i=0; i<12; i++){
-      $(spaces[i]).addClass('player2');
-    }
-    for(i=20; i<32; i++){
-      $(spaces[i]).addClass('player1');
+
+
+  function request(){
+    var answer = confirm('Are you sure you would like to reset?');
+    if(answer===true){
+      setUpBoard();
     }
   }
-
-  function reset(){
-
-  }
-
-
 
 
 
